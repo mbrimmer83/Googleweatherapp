2ff4084b3f458a3f498ae6062d6754d3a2777ebf
Matthew <mbrimmer1@gmail.com>
2016-06-09 16:57:06 -0400

    began adding forecast. Not complete.

--- weathermap.js
+++ weathermap.js
@@ -19,17 +19,36 @@ var cityIds = [
   5419384,
   4990729
 ];
-
-// var infoWindows = [];
-var app = angular.module('my-app', []);
+var cities = cityIds.join(",");
+var app = angular.module('my-app', ['ngRoute']);
 var markerDictionary = {};
-app.controller('MainController', function($scope, $http){
-  var map = new google.maps.Map(document.getElementById('map'), {
+// weather service factory.
+app.factory('weather', function($http) {
+  return {
+    getWeatherData: function(cities, callback) {
+      $http({
+        method: 'GET',
+        url: 'http://api.openweathermap.org/data/2.5/group?',
+        params: {
+          APPID: '24ccce52218267e8c0e1a0ba504e7ec7',
+          units: 'imperial',
+          id: cities
+        }
+      }).success(function(weatherMapData) {
+          console.log( weatherMapData);
+          callback(weatherMapData);
+        });
+    }
+  };
+});
+app.factory('googleMap', function() {
+  var mapElement = document.getElementById('map');
+  var map = new google.maps.Map(mapElement, {
     center: {lat: 39.99727, lng: -94.578567},
     zoom: 4
   });
   var infoWindow = new google.maps.InfoWindow();
-  $scope.openInfoWindow = function(results) {
+  function openInfoWindow(results) {
     var iconId = results.weather[0].icon;
     var cityName = results.name;
     var temperature = results.main.temp;
@@ -39,32 +58,18 @@ app.controller('MainController', function($scope, $http){
     var humidity = results.main.humidity;
     var windSpeed = results.wind.speed;
     var contentString =
-    '<p>' + cityName + '</br>Temp: ' + temperature + ' F</br>' + '</br>Hi-Temp: ' + hiTemperature + ' F</br>' + '</br>Low-Temp: ' + lowTemperature + ' F</br>' + '</br>Pressure: ' + pressure + '</br>' + '</br>Humidity: ' + humidity + '</br>' + '</br>Wind Speed: ' + windSpeed + ' mph</br>';
+    '<p>' + cityName + '</br>Temp: ' + temperature + ' F</br>' + '</br>Hi-Temp: ' + hiTemperature + ' F</br>' + '</br>Low-Temp: ' + lowTemperature + ' F</br>' + '</br>Pressure: ' + pressure + ' Pa</br>' + '</br>Humidity: ' + humidity + '</br>' + '</br>Wind Speed: ' + windSpeed + ' mph</br>' + '"<a href=#/city/>' + results.id"' 'Forecast</a>';
     var marker = markerDictionary[results.id];
     infoWindow.setContent(contentString);
     infoWindow.open(map, marker);
-  };
-  $http({
-    method: 'GET',
-    url: 'http://api.openweathermap.org/data/2.5/group?',
-    params: {
-      APPID: 'eac2948bfca65b78a8c5564ecf91d00e',
-      units: 'imperial',
-      id: cityIds.join(",")
-    }
-  })
-
-  .success(function(weatherMapData){
-    console.log( weatherMapData);
-    var results = weatherMapData.list;
-    $scope.results = results;
+  }
+  function makeMarkers(results) {
     var weatherData = results.map(function(results){
       var iconId = results.weather[0].icon;
       var position = {
         lat: results.coord.lat,
         lng: results.coord.lon
       };
-
       var iconBase = 'http://openweathermap.org/img/w/';
       var marker = new google.maps.Marker({
         position: position,
@@ -80,9 +85,62 @@ app.controller('MainController', function($scope, $http){
       });
       markerDictionary[results.id] = marker;
       marker.addListener('click', function(){
-        $scope.openInfoWindow(results);
+        openInfoWindow(results);
       });
       return marker;
     });
+  }
+  return {
+    openInfoWindow: openInfoWindow,
+    makeMarkers: makeMarkers
+  };
+});
+app.config(function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'overview.html',
+    controller: 'MainController'
+  })
+  .when('/forecast', {
+    templateUrl: 'forecast.html',
+    controller: 'ForecastController'
+  });
+});
+//Forecat data request factory.
+app.factory('weatherForecast', function($http) {
+  return {
+    getForecastData: function(cityId, callback) {
+      $http({
+        method: 'GET',
+        url: 'http://api.openweathermap.org/data/2.5/forecast',
+        params: {
+          APPID: '24ccce52218267e8c0e1a0ba504e7ec7',
+          units: 'imperial',
+          id: cityId
+        }
+      }).success(function(forecastData) {
+          console.log(forecastData);
+          callback(forecastData);
+        });
+    }
+  };
+});
+
+app.controller('MainController', function($scope, $http, weather, googleMap){
+  weather.getWeatherData(cities, function(weatherMapData){
+    var results = weatherMapData.list;
+    $scope.results = results;
+    googleMap.makeMarkers(results);
+  });
+  $scope.openInfoWindow = function(result) {
+    googleMap.openInfoWindow(result);
+  };
+});
+
+
+app.controller('ForecastController', function($scope, $http, weatherForecast, googleMap, $routeParams) {
+  var cityId = $routeParams.cityId;
+  weatherForecast.getForecastData(cityId, function(forecastData) {
+    var forecastResults = forecastData;
+    $scope.forecastResults = forecastResults.list;
   });
 });
